23fe694a9e8536583fffe37727c3d4ee73a40e0f
gotin <gotin_av@mgtniip.ru>
2022-10-24 21:15:19 +0300

    mergerequest changes

--- config.py
+++ config.py
@@ -10,5 +10,3 @@ APP_URL = 'https://gitlabnotifications.herokuapp.com/' + BOT_TOKEN
 # https://api.telegram.org/bot5684322124:AAFIZpLs2HgluqopmS5y9mnjVe5jTPylkIE/deleteWebhook?url=https://f0eb-195-245-205-157.eu.ngrok.io/tg
 # https://api.telegram.org/bot5684322124:AAFIZpLs2HgluqopmS5y9mnjVe5jTPylkIE/setWebhook?url=https://nextjs-eight-silk-57.vercel.app/tg
 # https://f0eb-195-245-205-157.eu.ngrok.io/tg
-
-print([(None)][0])
